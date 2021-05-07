fec4669005f8ebd4d3a130b37e0cf176c84a5010
GuiltyCat <37097820+GuiltyCat@users.noreply.github.com>
2021-05-07 23:08:07 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 ]
 setuptools.setup(
     name="salt-viewer",
-    version="0.1.7",
+    version="0.1.9",
     description="Simple (archived) image viewer",
     long_description=description,
     long_description_content_type="text/markdown",
